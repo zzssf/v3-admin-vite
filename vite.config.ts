709b81c50d672ea950a692b206c16a29cb525dac
付羽凡 <5288595+fu_yufan@user.noreply.gitee.com>
2024-05-19 22:22:59 +0800

    fix: 修改代理地址为本地代理地址

--- vite.config.ts
+++ vite.config.ts
@@ -35,7 +35,7 @@ export default ({ mode }: ConfigEnv): UserConfigExport => {
       /** 接口代理 */
       proxy: {
         "/api/v1": {
-          target: "https://mock.mengxuegu.com/mock/63218b5fb4c53348ed2bc212",
+          target: "http://localhost:3099/",
           ws: true,
           /** 是否允许跨域 */
           changeOrigin: true
